8fbf00600babd4a66acd0c14047043f55f109d2c
Vincent Kurniawan <6181901024@student.unpar.ac.id>
2022-05-07 00:59:04 +0700

    bug on server

--- GUI/server.py
+++ GUI/server.py
@@ -1,14 +1,24 @@
 # Nadia Clarissa Hermawan / 6181901013
 # we definitely dont need any GUI on the server side :)
 
+from functools import partial
 import socket, threading
 import time
 from _thread import *
+import tkinter as tk
 
 # score state collection
 SCORE_STATE_TRUE = 1
 SCORE_STATE_FALSE = 2
 SCORE_STATE_NOTSET = 3
+
+# answer collection
+ANSWER_A = 1
+ANSWER_B = 2
+ANSWER_C = 3
+ANSWER_D = 4
+
+window = tk.Tk()
 
 class Round():
     #constructor
@@ -35,8 +45,63 @@ class ClientThread(threading.Thread) :
         self.server_wait = True
         self.server_score = 0
         self.client_score = 0
-        self.exceed_time = False
+
+        self.server_answer = -100
+
         print("New connection added: ", self.caddress)
+        self.setupGUI()
+
+    def setupGUI (self):
+        self.window = window
+        self.window.title('KAHUUT !! (SERVER)')
+
+        #create the empty frame 
+        self.create_display_frame()
+        self.create_display_labels()
+
+    def create_display_frame(self):
+        self.frame_title = tk.Frame(self.window, relief='sunken')
+        self.frame_title.pack(fill='both', expand=True, padx=10, pady=10)
+
+        self.frame_button = tk.Frame(self.window, relief='sunken')
+        self.frame_button.pack(fill='both', expand=True, padx=10, pady=10)
+
+        self.frame_result = tk.Frame(self.window, relief='sunken')
+        self.frame_result.pack(fill='both', expand=True, padx=10, pady=10)
+
+
+    def create_display_labels(self):
+        self.title = tk.Label(self.frame_title, text='', font = ('Courier New', 18))
+        self.title.pack()
+
+        self.question = tk.Label(self.frame_title, text='', font = ('Courier New', 18))
+        self.question.pack()
+
+        self.answer_a = tk.Button(self.frame_button, text='', font = ('Courier New', 18), bg='#9FB4FF', command=partial(self.handler_answer_send, ANSWER_A))
+        self.answer_a.pack()
+
+        self.answer_b = tk.Button(self.frame_button, text='', font = ('Courier New', 18), bg='#9FB4FF', command=partial(self.handler_answer_send, ANSWER_B))
+        self.answer_b.pack()
+
+        self.answer_c = tk.Button(self.frame_button, text='', font = ('Courier New', 18), bg='#9FB4FF', command=partial(self.handler_answer_send, ANSWER_C))
+        self.answer_c.pack()
+
+        self.answer_d = tk.Button(self.frame_button, text='', font = ('Courier New', 18), bg='#9FB4FF', command=partial(self.handler_answer_send, ANSWER_D))
+        self.answer_d.pack()
+
+        self.score_state = tk.Label(self.frame_result, text='', font = ('Courier New', 18))
+        self.score_state.pack()
+
+        self.score = tk.Label(self.frame_result, text='', font = ('Courier New', 18))
+        self.score.pack()
+
+        self.status = tk.Label(self.frame_result, text='', font = ('Courier New', 18))
+        self.status.pack()
+
+    def handler_answer_send(self, answer):
+        self.answer = str(answer)
+        self.server_wait = False
+        self.status['text'] = 'Waiting for other player ...'
 
     #thread.start() akan menjalankan ini
     def run(self) :
@@ -69,91 +134,74 @@ class ClientThread(threading.Thread) :
                 self.csocket.send(str_question_score.encode('utf-8'))
 
                 for j in range (5, 0, -1):
-                    print ('Please wait ', j, ' seconds for next question ...')
+                    self.status.config(text='Please wait '+ str(j)+ ' seconds for next question ...')
+                    self.window.update()
                     time.sleep(1)
 
                 # open cui on server player
                 self.handler_question_score(i, rounds[i].question, rounds[i].a, rounds[i].b, rounds[i].c, rounds[i].d, self.server_score)
                 
                 self.server_wait = True
-                self.exceed_time = False
 
-                threading.Thread(target=self.server_wait_time).start()
-                answer = str(input())
-                if (self.exceed_time):
-                    answer = "INVALID"
-        
-                if (self.evaluate_answer(i, self.convert_answer(answer))):
-                        self.server_score += 100
-                        print ("\nYour Answer is Correct !\n")
-                else:
-                        print ("\nYour Answer is Wrong !\n")
-                print ("Waiting for other player ...")
-                self.server_wait = False
-                
-            i += 1
+                threading.Thread(target=self.server_wait_time, args=(i,)).start()
         
         # end game to client
         final_score = [self.server_score, self.client_score]
         str_final_score = "_".join(str(x) for x in final_score)
         self.csocket.send(str_final_score.encode('utf-8'))
 
-        # end game on server
-        print ("<==========================================>")
-        print ('FINAL SCORE:')
-        print ("<==========================================>")
-        print ('YOU : ', self.server_score, ' OTHER PLAYER : ', self.client_score)
-        print ("<==========================================>")
-        for i in range (10, 0, -1):
-            print ('The game will shut down in ', i, ' ...')
-            time.sleep(1)
-        print ("<==========================================>")
-        print("Client at ", self.caddress, " disconnected...")
+        if (self.client_score > self.server_score):
+            self.title['text'] = 'CONGRATS YOU WIN!'
+        elif (self.client_score < self.server_score):
+            self.title['text'] = 'YOU LOSE!'
+        else:
+            self.title['text'] = 'TIED GAME!'
 
-    def server_wait_time (self):
-        check = True
-        print ('You have 10 seconds left to answer ...')
+        self.question['text'] = 'your score : ' + str(self.server_score) + ' . other player score : ' + str(self.client_score)
+
+        self.answer_a['text'] = ''
+        self.answer_b['text'] = ''
+        self.answer_c['text'] = ''
+        self.answer_d['text'] = ''
+        self.score['text'] = ''
+        self.score_state['text'] = ''
+
         for i in range (10, 0, -1):
+            self.status['text'] = 'The game will shutdown in ' + str(i) + ' seconds ...'
+            self.window.update()
+            time.sleep(1)
+
+    def server_wait_time (self, question_number):
+        check = True
+        for i in range (10, 0, -1):
+            self.status['text'] = 'You have ' + str(i) + ' seconds left to answer'
             if (self.server_wait == False):
                 check = False
                 break
+            self.window.update()
             time.sleep(1)
         # kalau ga jawab dalam 10 detik:
         if (check):
-            print ("Your time is up! You didn't get any score :( ")
-            print ("Please input anything to proceed the next question ...")
-            self.exceed_time = True
+            self.answer = -100
+
+        if (self.evaluate_answer(question_number, self.answer)):
+                self.server_score += 100
+                self.score_state['text'] = 'Your Answer is Correct!'
+                self.score['text'] = 'SCORE : ' + str(self.server_score)
+        else:
+                self.score_state['text'] = 'Your Answer is Wrong!'
+                self.score['text'] = 'SCORE : ' + str(self.server_score)
 
     def handler_question_score (self, question_number, question, answer_a, answer_b, answer_c, answer_d, current_score):
+        self.title['text'] = 'Question ' + str(question_number + 1)
+        self.question['text'] = question
+        self.answer_a['text'] = answer_a
+        self.answer_b['text'] = answer_b
+        self.answer_c['text'] = answer_c
+        self.answer_d['text'] = answer_d
 
-        print ("<==========================================>")
-        print ("\nYour Current Score : ",current_score,'\n')
-        print ("<==========================================>")
-        print ("<==========================================>")
-        print ("\nQuestion ",question_number+1,'\n')
-        print ("<==========================================>")
-        print (question, '\n')
-        
-        print ("A: " + answer_a)
-        print ("B: " + answer_b)
-        print ("C: " + answer_c)
-        print ("D: " + answer_d + "\n")
-        print ("Type your answer (A/B/C/D) :\n")
-        
-
-    def convert_answer (self, answer):
-        answer = answer.lower()
-        if (answer == 'a'):
-            return 1
-        elif (answer == 'b'):
-            return 2
-        elif (answer == 'c'):
-            return 3
-        elif (answer == 'd'):
-            return 4
-        else:
-            return -100
-
+        self.score_state['text'] = ''
+        self.score['text'] = ''
 
     def evaluate_answer(self, question_number, answer):
         if (rounds[question_number].key == answer):
@@ -171,20 +219,17 @@ def main():
     print('Server started. Socket binded to port ', port)
 
     #put the socket into listening mode, 5 client at max
-    server.listen(5)
+    server.listen(1)
     print("socket is listening, waiting for client request..")
 
     #TCP = keep connections until client wants to exit
-    while True:
-        #establish connection with client
-        clientSocket, clientAddress = server.accept()  
+    #establish connection with client
+    clientSocket, clientAddress = server.accept()
 
-        #start new thread to do operations & logic for each client
-        newThread = ClientThread(clientAddress, clientSocket)
-        newThread.start()
-    
-    #turn off server
-    s.close()
+    #start new thread to do operations & logic for each client
+    newThread = ClientThread(clientAddress, clientSocket)
+    newThread.start()
+    window.mainloop()
 
 
 if __name__ == '__main__':
